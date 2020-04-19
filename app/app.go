68f6718695fc0693402a36edbcd13f90d5c731ac
Santiago Núñez-Cacho <santiago.nunezcacho@softonic.com>
2020-04-19 20:06:14 +0200

    first draft

---
+++ app/app.go
@@ -0,0 +1,173 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// App : here you tell us what App is
+type App struct {
+	Status
+	questionQueue chan Question
+	priority      int
+	Answer
+	answerQueue   chan Answer
+}
+
+// Answer : here you tell us what Answer is
+type Answer struct {
+	ID 	    string
+	Answer	string
+}
+
+// Status : here you tell us what Status is
+type Status struct {
+	Question
+	Status				string
+	TimeAnswered        time.Duration
+	mutex               sync.Mutex
+	Answer
+}
+
+// Question : here you tell us what Question is
+type Question struct {
+	ID       string
+	Question string
+	Priority int
+}
+
+// Ack : here you tell us what Ack is
+type Ack struct {
+	ID         string
+	Success    bool
+	AckMessage string
+}
+
+// PostAnswerAck : here you tell us what PostAnswerAck is
+type PostAnswerAck struct {
+	Success    bool
+	AckMessage string
+}
+
+// NewStatus : Constructor of status struct
+func NewStatus() *Status {
+	return &Status{}
+}
+
+// NewApp : here you tell us what NewApp is
+func NewApp() *App {
+	questionQueue := make(chan Question, 100)
+	answerQueue := make(chan Answer, 100)
+	return &App{
+		questionQueue: questionQueue,
+		answerQueue: answerQueue,
+	}
+}
+
+
+
+
+func newQuestion(priority int, question string) Question {
+
+	fmt.Println("accessing new Question")
+
+	ID := "2" // call random generation number here
+
+	q := Question{ID: ID, Question: question, Priority: priority}
+
+	fmt.Println("creating question:", q)
+
+	return q
+}
+
+// QuestionPost :
+func (a *App) QuestionPost(priority int, question string) Ack {
+
+	q := newQuestion(priority, question)
+
+	ack := Ack{
+		ID:         q.ID,
+		Success:    true,
+		AckMessage: "delivered",
+	}
+
+	go func() {
+
+		if priority == 1 {
+			a.questionQueue <- q
+		}
+
+	}()
+
+	return ack
+
+}
+
+// GetNext :
+func (a *App) GetNext() (Question, error) {
+
+	var q Question
+
+	select {
+	case q, ok := <-a.questionQueue:
+		if ok {
+			fmt.Printf("Value %v was read.\n", q)
+			return q, nil
+		}
+		fmt.Println("Channel closed!")
+		return q, errors.New("channel closed")
+
+	default:
+		fmt.Println("No value ready, moving on.")
+		return q, errors.New("Item does not exist")
+	}
+
+}
+
+// PostCsAnswer :
+func (a *App) PostCsAnswer(ID string, answer string) PostAnswerAck {
+
+	a.Answer.Answer = answer
+
+	ackpostanswered := PostAnswerAck{
+		Success: true,
+		AckMessage: "",
+	}
+
+	fmt.Println("Customer Service posting this answer:", a.Answer.Answer )
+
+	go func() {
+
+			a.answerQueue <- a.Answer
+
+
+	}()
+
+	return ackpostanswered
+
+}
+
+// GetStatus :
+func (a *App) GetStatus(param string) ( Status, error ) {
+
+	s := NewStatus() 
+
+	fmt.Println(a.answerQueue)
+
+	select {
+	case r, ok := <-a.answerQueue:
+		if ok {
+			fmt.Printf("Value %v was read.\n", r)
+			s.Answer = r
+			return *s, nil
+		}
+	default:
+		fmt.Println("No value ready, moving on.")
+		return *s, errors.New("Item does not exist")
+	}
+
+	return *s,nil
+
+}
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	app "github.com/santinoncs/GoCostumerSupportApi/app"
+	//"sync"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+// IncomingQuestion : here you tell us what IncomingQuestion is
+type IncomingQuestion struct {
+	Priority int    `json:"priority"`
+	Question string `json:"question"`
+}
+
+var application *app.App
+
+func main() {
+
+	application = app.NewApp()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r)
+	})
+	err := http.ListenAndServe(":8080", nil)
+
+	fmt.Println(err)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	var responseAck app.Ack
+	var responseQuestion app.Question
+	var responseAnswer app.PostAnswerAck
+	var responseStatus app.Status
+	var content IncomingQuestion
+	var postAnswer app.Answer
+
+	if r.URL.Path == "/api/question/post" {
+
+		err := json.NewDecoder(r.Body).Decode(&content)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		responseAck = application.QuestionPost(content.Priority, content.Question)
+		responseJSON, _ := json.Marshal(responseAck)
+		fmt.Fprintf(w, "Response: %s\n", responseJSON)
+
+	}
+
+	if r.URL.Path == "/api/question/get_next" {
+
+		responseQuestion, _ = application.GetNext()
+		responseJSON, _ := json.Marshal(responseQuestion)
+		fmt.Fprintf(w, "Response: %s\n", responseJSON)
+
+	}
+
+	if r.URL.Path == "/api/question/answer" {
+
+		err := json.NewDecoder(r.Body).Decode(&postAnswer)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		fmt.Println("the answer I will send to method:", postAnswer.Answer)
+
+		responseAnswer = application.PostCsAnswer(postAnswer.ID, postAnswer.Answer)
+		responseJSON, _ := json.Marshal(responseAnswer)
+		fmt.Fprintf(w, "Response: %s\n", responseJSON)
+
+	}
+
+	params, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+			log.Fatal(err)
+	}
+
+
+	if r.URL.Path == "/api/question/status" {
+
+		param := params.Get("question_id")
+
+		fmt.Println(param)
+
+		responseStatus, _ = application.GetStatus(param)
+		responseJSON, _ := json.Marshal(responseStatus)
+		fmt.Fprintf(w, "Response: %s\n", responseJSON)
+
+	}
+
+
+}
